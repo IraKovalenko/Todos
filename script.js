d59039a62b2096bcbf1487d1be9a949a4332e75b
Iryna Kovalenko <Iren_95@bk.ru>
2017-02-01 22:55:14 +0200

    todos fixes

--- script.js
+++ script.js
@@ -3,118 +3,72 @@ var list = document.getElementsByClassName("todo-list")[0];
 var inputField = document.getElementsByClassName("new-todo")[0];
 var emptyItem = document.getElementsByClassName("empty-item")[0].children[0];
 var counter = document.getElementsByClassName("todo-count")[0];
-var footer = document.getElementsByClassName("footer")[0];
-
-var updateCounter = function(){
-  counter.querySelector("strong").innerHTML = list.children.length;
-}
-updateCounter();
+var filters = document.getElementsByClassName("filters")[0];
+var clear = document.getElementsByClassName("clear-completed")[0];
 var todos = [];
 var todoCounter = 0;
+
+var updateCounter = function(){
+  counter.querySelector("strong").innerHTML = list.children.length+"";
+};
+updateCounter();
 
 function createTodo(text) {
   return {
     id: todoCounter++,
-    text: text, 
+    text: text,
     isCompleted: false
   }
 }
 
-function taskStatus(event){
-  if (event.target.classList.contains("destroy")) {
-    event.target.closest("li").remove();
-    todos.splice(event.target.closest("li").dataset.todoId, 1);
-    updateCounter();
-  } else{
-    event.target.closest("li").classList.toggle("completed");
-    if (event.target.closest("li").classList.contains("completed")) {
-      todos.forEach(function (todo) {
-        if (todo.id == event.target.closest("li").dataset.todoId) {
-          todo.isCompleted = true;
-        }
-      })}
-    else{
-      todos.forEach(function (todo) {
-        if (todo.id == event.target.closest("li").dataset.todoId) {
-          todo.isCompleted = false;
-        }
-      });
-    }
-  }
-
-}
-
-function renderTodos(filter) {
+function renderTodos() {
   list.innerHTML = '';
-
-  function cloneEl(todo) {
-    var cloned = emptyItem.cloneNode(true);
-    cloned.querySelector("label").textContent = todo.text;
-    cloned.setAttribute("data-todo-id", todo.id);
-    cloned.onclick = taskStatus;
-
-    if(todo.isCompleted) {
-      cloned.classList.add("completed");
-    }
-    list.appendChild(cloned);
-  }
-
-  if (!filter) {
-    todos.forEach(function(todo) {
-      cloneEl(todo);
-    })
-  } else if (filter === 'completed') {
-    todos.forEach(function(todo) {
+  var filter = filters.querySelector('.selected').dataset.filter;
+  todos.forEach(function(todo) {
+    if (checkFilter(todo, filter)){
+      var clone = emptyItem.cloneNode(true);
+      clone.querySelector("label").textContent = todo.text;
+      clone.dataset.id = todo.id;
       if (todo.isCompleted) {
-        cloneEl(todo);
+        clone.classList.toggle('completed');
       }
-    })
-  } else if (filter === 'active') {
-    todos.forEach(function(todo) {
-      if (!todo.isCompleted) {
-        cloneEl(todo);
-      }
-    })
-  }
+      list.appendChild(clone);
+    }
+  });
   updateCounter();
 }
 
+function checkFilter(todo, filter){
+  return filter=='all'||(filter == 'active' && !todo.isCompleted)||(filter=='completed'&&todo.isCompleted);
+}
 inputField.onkeyup = function addTask(event) {
-  if (event.keyCode === 13 ) {
+  if (event.keyCode === 13) {
     var task = inputField.value;
     todos.push(createTodo(task));
     renderTodos();
     inputField.value = "";
   }
-  updateCounter();
-}
-
-
-footer.onclick = function filterList(event){
-  var filters = footer.querySelectorAll('a');
-  function changeFilter() {
-    for (var i = 0; i < filters.length;  i++){
-      if (event.target === filters[i]) {
-        filters[i].classList.add('selected');
-      } else {
-        filters[i].classList.remove('selected');
-      }
-    }
-  }
-  if (event.target.dataset.filter === "all"){
-    changeFilter();
+};
+list.onclick = function taskStatus(event){
+  if (event.target.classList.contains("destroy")) {
+    delete todos[event.target.closest("li").dataset.id];
     renderTodos();
-  } else if (event.target.dataset.filter === "active"){
-    changeFilter()
-    renderTodos("active");
-  } else if (event.target.dataset.filter === "completed"){
-    changeFilter()
-    renderTodos("completed");
   }
-  if (event.target.classList.contains("clear-completed")){
-    todos = todos.filter(function(todo) {
-      return !todo.isCompleted
-    });
-    renderTodos(""+event.target.dataset.filter);
+  if (event.target.classList.contains("toggle")){
+    todos[event.target.closest("li").dataset.id].isCompleted = !todos[event.target.closest("li").dataset.id].isCompleted;
+    event.target.closest("li").classList.toggle("completed");
   }
-}
+};
+
+filters.onclick = function changeFilter(event) {
+  filters.querySelector('.selected').classList.remove('selected');
+  event.target.classList.add('selected');
+  renderTodos();
+};
+
+clear.onclick = function clearCompleted(){
+  todos.forEach(function(todo) {
+    if (todo.isCompleted) delete todos[todo.id];
+  });
+  renderTodos();
+};
